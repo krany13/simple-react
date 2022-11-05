dd0254c91d3a7910ef467bea007484ade583275a
krany13 <krasniy.vladim@yandex.ru>
2022-11-06 01:09:49 +0300

    before the start of studies

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+}
+
+export function Accordion(props: AccordionPropsType) {
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+export function AccordionTitle(props: AccordionTitlePropsType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+export function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
