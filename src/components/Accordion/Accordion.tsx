d0ce257431b586a74b332c6479eabfa955e5acd5
krany13 <krasniy.vladim@yandex.ru>
2022-11-18 17:43:07 +0300

    Conditional rendering

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 type AccordionPropsType = {
     titleValue: string
+    collapsed?: boolean
 }
 
 export function Accordion(props: AccordionPropsType) {
     return (
         <div>
             <AccordionTitle title={props.titleValue}/>
-            <AccordionBody/>
+            {!props.collapsed && <AccordionBody/>}
         </div>
     )
 }
